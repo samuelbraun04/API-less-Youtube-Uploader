4e2054312cb0ba93a768f2c8df4932d6389458e4
Samuel Braun <samuelbraun17@gmail.com>
2023-03-31 16:14:57 -0400

    minor change to naming

---
+++ README.md
@@ -0,0 +1,19 @@
+# API-less YouTube Uploader
+
+This project provides a way to upload videos to YouTube without using the YouTube API. It uses the YouTube web interface to perform the upload, so it can be used in cases where the YouTube API is not available or cannot be used.
+
+## Getting Started
+
+To use the API-less YouTube Uploader, you will need to download and install the following software:
+
+- [Java Development Kit (JDK) version 8 or higher](https://www.oracle.com/ca-en/java/technologies/downloads/)
+- [Selenium for Java](https://www.selenium.dev/downloads/)
+
+## Usage
+
+Once you have these dependencies installed, you can build the project using the following command:
+
+
+```
+java Youtu <path-to-video-file> <youtube-username> <youtube-password> <video-title> <video-description>
+```
--- src/YoutubeUploader.java
+++ src/YoutubeUploader.java
@@ -1,20 +1,20 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.time.*;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.ElementNotInteractableException;
 import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import java.util.List;
-import java.util.Random;
-import java.time.*;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
   
-public class YoutubeDownloader { 
+public class YoutubeUploader { 
 	
 	//create random instance
 	private static Random rand;
